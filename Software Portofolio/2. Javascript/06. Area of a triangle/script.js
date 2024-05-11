7f14c7c04a7fc9ecdce3a3151f34ac5f94874144
PaulNegrea <103375900+PaulNegrea91@users.noreply.github.com>
2024-05-11 17:57:01 +0300

    Delete Software Portofolio/2. Javascript directory

--- Software Portofolio/2. Javascript/06. Area of a triangle/script.js
+++
@@ -1,7 +0,0 @@
-const areaElement  = document.getElementById("areaTriangle")
-const a = 452;
-const b = 782;
-const c = 895;
-const sp = (a + b + c) / 2
-const area = Math.sqrt(sp * (sp - a) * (sp - b) * (sp - c));
-areaElement.innerHTML = area;
